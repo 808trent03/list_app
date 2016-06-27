62f05410dcd5229606e88afe42f9d998d3500480
808trent03 <thamura22@punahou.edu>
2016-06-27 11:56:13 -1000

    added html and js
    added html and js

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var arr = ['red', 'orange', 
+'yellow', 'green', 'blue', 'indigo', 
+ 'violet'];
+ var count = 0;
+
+var cycle = function(array/*perameter*/) {
+	console.log(array[count]);
+	
+
+	console.log(array[count]);
+	count++; // or count += 1; or count = count + 
+};
+
+var list3 = function() {
+	cycle(arr);
+}
