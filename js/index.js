e28f79a8cb2a075ed001020d6c3b94955084e200
808trent03 <thamura22@punahou.edu>
2016-06-28 11:55:05 -1000

    added panic button
    added panic button

--- js/index.js
+++ js/index.js
@@ -1,16 +1,27 @@
-var arr = ['red', 'orange', 
-'yellow', 'green', 'blue', 'indigo', 
- 'violet'];
+var arr = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"
+];
  var count = 0;
 
-var cycle = function(array/*perameter*/) {
+var cycle = function(array) {
 	console.log(array[count]);
+	if (count == array.length-1){
+		count = 0;
+	} else {
+		count++;
+	};
 	
-
-	console.log(array[count]);
-	count++; // or count += 1; or count = count + 
 };
 
-var list3 = function() {
-	cycle(arr);
+var display = function(){
+	document.body.style.color = stuff[count];
+	cycle(stuff);
+
+};
+
+var listAll = function(array) {
+	for (var i = 0; i < array.length; i++) {
+		console.log(i)
+		document.getElementById('stuff').innerHTML += "<li>" + array[i] + "</li>";
+	}
 }
+listAll(stuff)
